d2569da3fbadd1f974553438f36b5d2f0571d2dd
dsani <dsani@outlook.com>
2014-05-23 01:05:10 -0500

    r-assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Put comments here that give an overall description of what your
+# functions do
 
-## Write a short comment describing this function
+# Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  ## Set matrix function
+  # Set matrix function
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  ## Get matrix function
+  # Get matrix function
   get <- function() x
-  ## Setting inverse matrix cache function
+  # Setting inverse matrix cache function
   setinv <- function(minv) inv <<- minv
-  ## Get cached matrix function
+  # Get cached matrix function
   getinv <- function() inv
-  ## Returns cachematrix data type 
+  # Returns cachematrix data type 
   list(set = set, get = get, 
        setinv = setinv,
        getinv = getinv) 
 }
 
 
-## Write a short comment describing this function
+# Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  # Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
-  ## Check if matrix has already been cached
+  # Check if matrix has already been cached
   if(!is.null(inv)) {
     message("getting cached matrix")
     return(inv)
   }
-  ## Get matrix
+  # Get matrix
   data <- x$get()
-  ## Solve matrix
+  # Solve matrix
   inv <- solve(data, ...)
-  ## Set cache
+  # Set cache
   x$setinv(inv)
-  ## Return inverse matrix
+  # Return inverse matrix
   inv
 }
